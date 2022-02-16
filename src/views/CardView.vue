d143cd22e5658d0f361c48e87759b8a64465b3c6
Alen Dosev <alen@apptitude.nl>
2022-02-16 14:53:38 +0100

    more things

---
+++ src/views/CardView.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts"></script>
+
+<script lang="ts">
+export default {
+  data: () => ({
+    text: "",
+    users: [],
+  }),
+  async mounted() {
+    const { data } = await this.axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    const google = await this.axios.get(
+      "https://sheets.googleapis.com/v4/spreadsheets/1EHBijDmk5nIANiYF4aiUViKKNPfKfgdw-Ws4B8Xt5mA/values/H1!A1:D5?key=AIzaSyDXokXhuuI6PRqUDAMw1dDhOKXlFHcWDh0"
+    );
+    // const google = await this.axios.get(
+    //   "https://opensheet.elk.sh/1EHBijDmk5nIANiYF4aiUViKKNPfKfgdw-Ws4B8Xt5mA/1"
+    // );
+    this.users = data;
+    console.log(google);
+  },
+};
+</script>
+
+<template>
+  <main>
+    <h1>HELLOO</h1>
+    <ul>
+      <li v-for="user in users" :key="user.id">{{ user.name }}</li>
+    </ul>
+  </main>
+</template>
